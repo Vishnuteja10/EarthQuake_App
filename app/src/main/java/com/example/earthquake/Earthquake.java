13f4d62302945b5e321f4b9a4bd1e3ac6aaff06c
Vishnuteja <vishnuteja476@gamil.com>
2021-06-04 18:18:58 +0530

    till loaders

--- app/src/main/java/com/example/earthquake/Earthquake.java
+++ app/src/main/java/com/example/earthquake/Earthquake.java
@@ -1,49 +1,110 @@
 package com.example.earthquake;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager.LoaderCallbacks;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Earthquake extends AppCompatActivity {
+public class Earthquake extends AppCompatActivity implements LoaderCallbacks<List<Quake>> {
 
     private static final String USGS_REQ_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10" ;
 
     private EarthquakeAdapter mAdapter;
+
+    private static final int earthquake_loader_id = 1;
+    private TextView mEmptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_earthquake );
 
-       mAdapter = new EarthquakeAdapter( this,new ArrayList<Quake>() );
+        mAdapter = new EarthquakeAdapter( this, new ArrayList<Quake>() );
         ListView listView = findViewById( R.id.list );
+        mEmptyStateTextView = (TextView) findViewById( R.id.empty_view );
+        listView.setEmptyView( mEmptyStateTextView );
         listView.setAdapter( mAdapter );
+
 
         listView.setOnItemClickListener( new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Quake currearthquake = mAdapter.getItem( position );
-                Uri earthquakeUri = Uri.parse(currearthquake.getUrl());
-                Intent websiteIntent =new Intent(Intent.ACTION_VIEW,earthquakeUri);
+                Uri earthquakeUri = Uri.parse( currearthquake.getUrl() );
+                Intent websiteIntent = new Intent( Intent.ACTION_VIEW, earthquakeUri );
                 startActivity( websiteIntent );
 
             }
         } );
 
-        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
-        task.execute(USGS_REQ_URL);
+      /*  EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQ_URL); */
+        // LoaderManager loaderManager = getLoaderManager();
+        //loaderManager.initLoader( earthquake_loader_id,null,this );
+
+        ConnectivityManager conmag = (ConnectivityManager)
+                getSystemService( Context.CONNECTIVITY_SERVICE );
+        NetworkInfo networkInfo = conmag.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+            androidx.loader.app.LoaderManager loaderManager1 = null;
+            loaderManager1.getInstance( this ).initLoader( earthquake_loader_id, null, this );
+        } else {
+            View loadingindicator = findViewById( R.id.loading_indicator );
+            loadingindicator.setVisibility( View.GONE );
+
+            mEmptyStateTextView.setText( R.string.no_internet );
+        }
+    }
+
+    @NonNull
+
+    @Override
+    public androidx.loader.content.Loader<List<Quake>> onCreateLoader(int id, @Nullable  Bundle args) {
+        EarthquakeLoader earthquakeLoader = new EarthquakeLoader( this,USGS_REQ_URL );
+       return earthquakeLoader;
+    }
+
+    @Override
+    public void onLoadFinished( androidx.loader.content.Loader<List<Quake>> loader, List<Quake> data) {
+
+        View loadingIndicator = findViewById( R.id.loading_indicator );
+        loadingIndicator.setVisibility( View.GONE );
+
+        mEmptyStateTextView.setText(R.string.no_earthquakes);
+
+        mAdapter.clear();
+
+        if(data != null && !data.isEmpty()){
+            mAdapter.addAll( data );
+        }
 
     }
-    private class EarthquakeAsyncTask extends AsyncTask<String,Void, List<Quake> > {
+
+    @Override
+    public void onLoaderReset( androidx.loader.content.Loader<List<Quake>> loader) {
+
+        mAdapter.clear();
+    }
+
+
+
+
+   /* private class EarthquakeAsyncTask extends AsyncTask<String,Void, List<Quake> > {
 
         @Override
         protected List<Quake> doInBackground(String... urls) {
@@ -59,6 +120,9 @@ public class Earthquake extends AppCompatActivity {
             if(data != null && !data.isEmpty()){
                 mAdapter.addAll( data );
             }
-        }
-    }
+
+       }
+       }
+
+    */
 }
