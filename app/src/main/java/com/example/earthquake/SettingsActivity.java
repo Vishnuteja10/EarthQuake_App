a91b3299f88990ab946885b6216f379e8fc7b30b
Vishnuteja <vishnuteja476@gamil.com>
2021-06-08 12:09:53 +0530

    fragmetns 2

--- app/src/main/java/com/example/earthquake/SettingsActivity.java
+++ app/src/main/java/com/example/earthquake/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.earthquake;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,11 +15,28 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_settings );
     }
-    public static class EarthQuakePreferenceFragment extends PreferenceFragment{
+    public static class EarthQuakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener{
 
         public void onCreate(Bundle savedInstanceState){
             super.onCreate( savedInstanceState );
             addPreferencesFromResource( R.xml.settings_main);
+
+            Preference minMagnitude = findPreference( getString( R.string.settings_min_magnitude_key ) );
+            bindPreferenceSummaryToValue(minMagnitude);
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener( this );
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences( preference.getContext() );
+            String prferenceString = preferences.getString( preference.getKey(),"" );
+            onPreferenceChange( preference,prferenceString );
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringvalue = value.toString();
+            preference.setSummary( stringvalue );
+            return true;
         }
     }
 }
